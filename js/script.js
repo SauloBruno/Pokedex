26ce66726cfe4789501296ae176fdbd619f59c87
Saulo_Calixto <brunocalisto9@gmail.com>
2023-06-15 15:52:18 -0300

    concluindo botões prev e next

--- js/script.js
+++ js/script.js
@@ -4,30 +4,63 @@ const pokemonImage = document.querySelector('.pokemon_image');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
 
 
 const fetchPokemon = async(pokemon) => {
 
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
+    
+    if(APIResponse.status == 200){
+        const data = await APIResponse.json();
+        return data;
+    }
+    
 }
 
-
 const renderPokemon = async(pokemon) => {
+
+    pokemonNumber.innerHTML = '00';
+    pokemonName.innerHTML = 'Carregando...'
+
     const data = await fetchPokemon(pokemon);
-
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-
+    
+    if(data)
+    {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        input.value = '';
+    }
+    else
+    {
+        //#acultar imagem caso pokemon não seja encontrado!!!
+        //pokemonImage.style.display = '###';
+        pokemonNumber.innerHTML = '00';
+        pokemonName.innerHTML = 'Not found :c'
+        input.value = '';
+    }
 }
 
 form.addEventListener('submit', (event)=>{
-
     event.preventDefault();
-    renderPokemon(input.value);
-
+    renderPokemon(input.value.toLowerCase());
 });
+
+buttonPrev.addEventListener('click', ()=>{
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+});
+
+buttonNext.addEventListener('click', ()=>{
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+
+renderPokemon(searchPokemon)
 
 
